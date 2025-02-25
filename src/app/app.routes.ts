ca0f7931e74a20d337606ce707503ff519a5d5a1
vivek <yadavvivek7698@gmail.com>
2025-02-25 15:55:00 +0530

    added the cart and change the default port

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { LoginComponent } from './auth/login/login.component';
 import { TopBarComponent } from './navigationBar/top-bar/top-bar.component';
 import { HomeComponent } from './home/home.component';
 import { AppComponent } from './app.component';
+import { CartComponent } from './cart/cart.component';
 
 export const routes: Routes = [
     // {path:"login" , component:LoginComponent}, 
     {path:"" , component:AppComponent},
     {path:"login",component:LoginComponent},
-    {path:"home",component:HomeComponent}
+    {path:"home",component:HomeComponent},
+    {path:"cart",component:CartComponent}
 ];
